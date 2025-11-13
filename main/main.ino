78895b11a94de9d37f304cef9188e08d04ebee92
Jacob <moujac@gmail.com>
2025-11-13 09:24:46 +0100

    added photocell and battery calc behavior

--- main/main.ino
+++ main/main.ino
@@ -9,12 +9,39 @@
 // Battery voltage sensor = A1
 // GPIO Extender (maybe redundant) = A4, A5
 
-void setup() {
-  // put your setup code here, to run once:
+const int led = 5;
+const int photoRes = A0;
+const int voltPin = A1;
 
+const float Vcc = 3.3;
+const float R1 = 10000.0;   
+const float R2 = 10000.0;   
+
+int photoVal = 0; // 0 to 1023
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(photoRes, INPUT);
+  pinMode(voltPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  // Calc battery voltage
+  int rawVolt = analogRead(voltPin);
+  float VoutVolt = (rawVolt / 1023.0) * Vcc;
+  float Vcap = VoutVolt * (R1 + R2) / R2;
+
+  // Handle PhotoResistor behavior
+  photoVal = analogRead(photoRes);
+  if(photoVal < 512){ // Need to figure out how value relates to light level
+    // It's dark, turn on LED(s)?
+    digitalWrite(led, HIGH);
+  }else{
+    // It's light, turn off LED(s)?
+    digitalWrite(led, LOW);
+  }
+
+  // Go sleep?
+  delay(1000);
 }
