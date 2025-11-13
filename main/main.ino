8ddb26420ba1e51c0b798dc6307b20cb3e9bc56d
Jacob <moujac@gmail.com>
2025-11-13 09:02:32 +0100

    added main, also defined used pins

---
+++ main/main.ino
@@ -0,0 +1,20 @@
+// PIN Allocation:
+// LoraWan (RN2483A) = D10, D11, D12 (As previusly used in class)
+// GPS Tx & Rx = D3, D4
+// LED(s) = D5
+// Speaker = D6
+// SWITCH (Button) = D7
+// Ultra sonic = D8, D9
+// Photocell = A0
+// Battery voltage sensor = A1
+// GPIO Extender (maybe redundant) = A4, A5
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
